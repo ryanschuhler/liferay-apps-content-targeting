f3188438b875cb28c2996084161d69d67951c8e8
Rebecca Dai <rebecca.dai@liferay.com>
2015-11-05 15:29:27 -0800

    add buyer stage rule

---
+++ rule-buyer-stage/src/com/liferay/content/targeting/rule/buyer/stage/BuyerStageRule.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.rule.buyer.stage.BuyerStage;
+
+import com.liferay.content.targeting.anonymous.users.model.AnonymousUser;
+import com.liferay.content.targeting.api.model.BaseRule;
+import com.liferay.content.targeting.api.model.Rule;
+import com.liferay.content.targeting.model.RuleInstance;
+import com.liferay.content.targeting.rule.categories.UserAttributesRuleCategory;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Brian Chan
+ */
+@Component(immediate = true, service = Rule.class)
+public class BuyerStageRule extends BaseRule {
+
+	@Activate
+	@Override
+	public void activate() {
+		super.activate();
+	}
+
+	@Deactivate
+	@Override
+	public void deActivate() {
+		super.deActivate();
+	}
+
+	@Override
+	public boolean evaluate(
+			HttpServletRequest request, RuleInstance ruleInstance,
+			AnonymousUser anonymousUser)
+		throws Exception {
+
+		String userBuyerStage = getUserBuyerStage(anonymousUser);
+
+		String buyerStage = ruleInstance.getTypeSettings();
+
+		if (Validator.equals(userBuyerStage, buyerStage)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public String getIcon() {
+		return "icon-usd";
+	}
+
+	@Override
+	public String getRuleCategoryKey() {
+		return UserAttributesRuleCategory.KEY;
+	}
+
+	@Override
+	public String getSummary(RuleInstance ruleInstance, Locale locale) {
+		return LanguageUtil.get(locale, ruleInstance.getTypeSettings());
+	}
+
+	public String getUserBuyerStage(AnonymousUser anonymousUser) {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String processRule(
+		PortletRequest request, PortletResponse response, String id,
+		Map<String, String> values) {
+
+		return values.get("buyer-stage");
+	}
+
+	@Override
+	protected void populateContext(
+		RuleInstance ruleInstance, Map<String, Object> context,
+		Map<String, String> values) {
+
+		String buyerStage = StringPool.BLANK;
+
+		if (!values.isEmpty()) {
+			buyerStage = values.get("buyer-stage");
+		}
+		else if (ruleInstance != null) {
+			buyerStage = ruleInstance.getTypeSettings();
+		}
+
+		context.put("buyerStage", buyerStage);
+	}
+
+}
+
